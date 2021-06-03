1d1155d0be07a7eb1308c497e2386983952dc624
abhilashshettigar <abhilashshettigar@gmail.com>
2021-06-03 13:31:57 +0530

    Inside footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,10 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-      <script src="https://p-visitor-tracking.s3.ap-south-1.amazonaws.com/tracking.js" id="traek_script" data-api="StQxrpstpijz0xX1ijNNJIfm9Ht1Ppj8"></script>
+      
       <footer className={styles.footer}>
         Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+       <script src="https://p-visitor-tracking.s3.ap-south-1.amazonaws.com/tracking.js" id="traek_script" data-api="StQxrpstpijz0xX1ijNNJIfm9Ht1Ppj8"></script>
       </footer>
     </>
   )
