50a1750d01f410700c21bfa159912c97034b2b43
abhilashshettigar <abhilashshettigar@gmail.com>
2021-06-03 13:24:10 +0530

    Adding script to footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,6 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
+      <script src="https://p-visitor-tracking.s3.ap-south-1.amazonaws.com/tracking.js" id="traek_script" data-api="StQxrpstpijz0xX1ijNNJIfm9Ht1Ppj8"></script>
         Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
       </footer>
     </>
