915c053eaced8ea812756c299a843acd47e23b87
abhilashshettigar <abhilashshettigar@gmail.com>
2021-06-03 13:40:01 +0530

    adding script to index file

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,6 @@ export default function Footer() {
       
       <footer className={styles.footer}>
         Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
-       <script src="https://p-visitor-tracking.s3.ap-south-1.amazonaws.com/tracking.js" id="traek_script" data-api="StQxrpstpijz0xX1ijNNJIfm9Ht1Ppj8"></script>
       </footer>
     </>
   )
